df2b88c3c69ee5c344b6112c784cb537818c87e4
Leonard304 <167664878+Leonard304@users.noreply.github.com>
2026-02-14 12:40:25 +0800

    added new file

--- caffeine_corner/online_shop/models.py
+++ caffeine_corner/online_shop/models.py
@@ -32,6 +32,7 @@ class Variant(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='variants')
     size = models.CharField(max_length=20, choices=SIZE_CHOICES)
     additional_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    sku = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return f"{self.product.name} - {self.size}"
