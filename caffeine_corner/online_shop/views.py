3e81ec64caf466c0b18e8bbc14ec158e1c09cad6
Leonard304 <167664878+Leonard304@users.noreply.github.com>
2026-02-15 15:43:18 +0800

    added new file

--- caffeine_corner/online_shop/views.py
+++ caffeine_corner/online_shop/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Product, Category, Variant
 
 # Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export const content = [
+    "./templates/**/*.html",
+    "./*/templates/**/*.html",
+    "./**/templates/**/*.html",
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
+
