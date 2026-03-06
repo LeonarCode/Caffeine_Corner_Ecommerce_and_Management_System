30dc50cfdc189c5dc0b9831b43f8fd554d9fa8ad
Leonard304 <167664878+Leonard304@users.noreply.github.com>
2026-03-06 12:49:21 +0800

    added new file

--- caffeine_corner/registration/urls.py
+++ caffeine_corner/registration/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('email-signup/', views.email_signup, name='email_signup'),
     path('logout/', views.logout_view, name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='authentication/password_reset.html', email_template_name='authentication/password_reset_email.txt', subject_template_name='authentication/password_reset_subject.txt', success_url=reverse_lazy('password_reset_done')), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='authentication/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='authentication/password_reset_confirm.html', success_url=reverse_lazy('password_reset_complete')), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='authentication/password_reset_complete.html'), name='password_reset_complete'),
 ]
